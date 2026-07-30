27549d0bf3a6b991323ef113e3da791c251e2ef9
dkondin-ccdc <dkondinskaia@ccdc.cam.ac.uk>
2026-07-30 15:03:43 +0000

    no_jira formatting fixed

--- scripts/crystallographic_flexibility.py
+++ scripts/crystallographic_flexibility.py
@@ -2,7 +2,9 @@
     molecule_flexibility.py - for computing crystallographic flexibility measures for molecules
 """
 ##########################################################################
+from packaging.version import Version
 
+import ccdc
 from ccdc.utilities import _private_importer
 
 with _private_importer() as pi:
@@ -21,11 +23,7 @@ class CrystallographicFlexibility:
     def __init__(self, output_dir):
 
         self.output_dir_ = output_dir
-
-        import ccdc
-        from packaging.version import Version
-        ccdc_version = Version(ccdc.__version__)
-        if ccdc_version >= Version("3.7.0"):
+        if Version(ccdc.__version__) >= Version("3.7.0"):
             self.calculator = ConformerGeneratorLib.MoleculeFlexibility(output_dir, False)
         else:
             self.calculator = ConformerGeneratorLib.MoleculeFlexibility(output_dir)
